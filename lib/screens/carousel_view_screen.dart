a0d616053230ef7fb8d4306747864d7e34214b3d
azix khan <azixkhan.55@gmail.com>
2024-08-10 17:00:53 +0500

    generation imaged and scrolled on carousel view widget

--- lib/screens/carousel_view_screen.dart
+++ lib/screens/carousel_view_screen.dart
@@ -12,17 +12,42 @@ class _CarouselViewScreenState extends State<CarouselViewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("New Carousel View Widget"),
-        elevation: 0,
+        title: const Text(
+          "New Carousel View Widget",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Column(
         children: [
           const SizedBox(
             height: 30,
           ),
-          const SizedBox(
-            height: 234,
-          )
+          SizedBox(
+            height: 240,
+            child: CarouselView(
+              backgroundColor: Colors.grey[300],
+              itemExtent: MediaQuery.of(context).size.height - 32,
+              elevation: 12.0,
+              overlayColor: const WidgetStatePropertyAll<Color>(Colors.black),
+              children: List.generate(
+                33,
+                (int index) {
+                  return Container(
+                    color: Colors.grey,
+                    child: Image.network(
+                      'https://picsum.photos/400?random=$index',
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
